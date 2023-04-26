df61a73a5880d4ca06dd20b80ec7096928a1e0fe
nirmalamondal <nirmalamondal2002@.com>
2023-04-26 12:53:44 +0530

    My first code

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>"
+
+@app.route("/hello_world1")
+def hello_world1():
+    return "<h1>Hello, World1!</h1>"
+
+@app.route("/hello_world2")
+def hello_world2():
+    return "<h1>Hello, World2!</h1>"    
+
+@app.route("/test")
+def test():
+    ans = 18+7
+    return "Hello Nirmala {}".format(ans)
+
+
+@app.route("/test2")
+def test2():
+    data = request.args.get('x')
+    return '''This is data input from my url{}'''.format(data)
+
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
